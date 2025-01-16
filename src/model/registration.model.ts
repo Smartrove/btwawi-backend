494492ce54ffe4089bcdd3ec870382808cb35e74
Lateef Wasiu Olalekan <108435744+Smartrove@users.noreply.github.com>
2025-01-16 11:40:56 +0100

    update on volunteer registration

---
+++ src/model/registration.model.ts
@@ -0,0 +1,85 @@
+import mongoose from "mongoose";
+import { EDITION_TYPE, USER_TYPE } from "../utils/Constants";
+
+export interface RegistrationDocument extends mongoose.Document {
+  fullName: string;
+  email: string;
+  phoneNum: string;
+  location: string;
+  committee: string;
+  interest: string;
+  skills: string;
+  hoursPerWeek: string;
+  meetingAvailability: string;
+  confidentiality: string;
+  additionalComments?: string;
+  createdAt: Date;
+  updatedAt: Date;
+}
+
+const registrationSchema = new mongoose.Schema(
+  {
+    fullName: {
+      type: String,
+      required: true,
+    },
+    phoneNum: {
+      type: String,
+      required: true,
+      unique: true,
+    },
+    email: {
+      type: String,
+      required: true,
+      unique: true,
+    },
+    location: {
+      type: String,
+      required: true,
+    },
+    committee: {
+      type: String,
+      required: true,
+    },
+    interest: {
+      type: String,
+      required: true,
+    },
+    skills: {
+      type: String,
+      required: true,
+    },
+    hoursPerWeek: {
+      type: String,
+      required: true,
+    },
+    attendingAs: {
+      type: String,
+      required: true,
+    },
+    howDidYouHear: {
+      type: String,
+      required: true,
+    },
+    meetingAvailability: {
+      type: String,
+      required: true,
+    },
+    confidentiality: {
+      type: String,
+      required: true,
+    },
+    additionalComments: {
+      type: String,
+      required: false,
+    },
+  },
+  { timestamps: true }
+);
+
+const Registration = mongoose.model<RegistrationDocument>(
+  "registration",
+  registrationSchema
+);
+
+export { Registration };
